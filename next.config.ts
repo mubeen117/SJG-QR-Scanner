5206481d13af46ea77a01fa30dc653ba52461731
saddam <62154427+saddamlakho@users.noreply.github.com>
2024-12-07 10:09:30 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,11 @@
+    // import type { NextConfig } from "next";
+
+    // const nextConfig: NextConfig = {
+    // /* config options here */
+    // };
+
+    // export default nextConfig;  
+   
    import type { NextConfig } from 'next';
    const nextConfig: NextConfig = {
    reactStrictMode: true,
